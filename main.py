2ca7abbeb6ec6d6491a5988234ec384fa704b683
EetuSeppa <eetu.seppanen99@gmail.com>
2024-09-23 13:29:23 +0300

    Read git clone exit status and output continuously
    Github authentication is still an issue. This can happen if multiple
    clones are made in close succession. Need to agree on how github auth
    should be handled.

--- main.py
+++ main.py
@@ -17,18 +17,25 @@ def clone_mine_and_delete(url: str):
     """
 
     #Clone repo
+    clone_output = ""
     p = subprocess.Popen(
         ["git", "clone", url],
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
+        stderr=subprocess.STDOUT,
+        universal_newlines=True
     )
 
-    try:
-        directory_name = re.search("Cloning into '(.*)'", str(p.stdout.read())).group(1)
-    except IndexError:
+    for line in iter(p.stdout.readline, ""):
+        print(line)
+        clone_output += line
+    p.stdout.close()
+    exit_code = p.wait()
+
+    if int(exit_code) != 0:
         print("Problem while cloning: " + url)
 
         return False
+    directory_name = re.search("Cloning into '(.*)'", clone_output).group(1)
 
     mine_repo(directory_name)
 
