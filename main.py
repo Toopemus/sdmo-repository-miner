a4233a07419ce9731c79883e4e40d00308bddb2b
EetuSeppa <eetu.seppanen99@gmail.com>
2024-09-24 15:00:46 +0300

    Use context manager for repositories

--- main.py
+++ main.py
@@ -2,48 +2,22 @@ import csv
 import subprocess
 import re
 import os
+from repository import Repository
 
 def mine_repo(directory:str):
     os.chdir(directory) #Change directory to git repository. Remove if not needed
 
     #Do mining here
-
-    os.chdir("..") #Remove if initial cd wasn't needed
-
-
-def clone_mine_and_delete(url: str):
-    """
-    Clone a repo, mine for refactoring efforts and remove the commit
-    """
-
-    #Clone repo
-    clone_output = ""
+    #Print commit history for now
     p = subprocess.Popen(
-        ["git", "clone", url],
+        ["git", "log"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True
     )
+    print(p.stdout.read())
 
-    for line in iter(p.stdout.readline, ""):
-        print(line)
-        clone_output += line
-    p.stdout.close()
-    exit_code = p.wait()
-
-    if int(exit_code) != 0:
-        print("Problem while cloning: " + url)
-
-        return False
-    directory_name = re.search("Cloning into '(.*)'", clone_output).group(1)
-
-    mine_repo(directory_name)
-
-    #Remove repo
-    p = subprocess.Popen(["rm", "-rf", directory_name])
-    p.wait(5)
-
-    return True
+    os.chdir("..") #Remove if initial cd wasn't needed
     
 def find_unique_projects(csv):
     projects = set()
@@ -65,7 +39,8 @@ def main():
         projects = find_unique_projects(reader)
         print("There are", len(projects), "unique projects")
     for project in projects:
-        clone_mine_and_delete(to_url(project))
+        with Repository(to_url(project)) as dir_name:
+            mine_repo(dir_name)
         input("Mined a repository, newline to continue") #Input to reduce spam, remove when not needed
 
 if __name__ == "__main__":
