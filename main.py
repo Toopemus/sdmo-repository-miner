fa528e1852fad92fee7dbb614daa11819e439e6e
EetuSeppa <eetu.seppanen99@gmail.com>
2024-10-09 13:14:56 +0300

    Added mining with refactoringminer
    Need to get refactorings in the correct format next

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import subprocess
 import os
 import urlparser
+import docker
 from repository import Repository
 
 def mine_repo(directory:str):
+    '''
+    Get commit hashes here if needed
     os.chdir(directory) #Change directory to git repository. Remove if not needed
-
-    #Do mining here
-    #Print commit history for now
     p = subprocess.Popen(
         ["git", "log"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True
     )
-    print(p.stdout.read())
-
+    #print(p.stdout.read())
     os.chdir("..") #Remove if initial cd wasn't needed
+    '''
+
+    #Do mining here
+    client = docker.from_env()
+    dir_real_path = os.path.realpath(directory)
+    miner = client.containers.run(
+        "tsantalis/refactoringminer", "-a " + "/repo",
+        volumes={
+            os.path.realpath(directory): {"bind": "/repo", "mode": "rw"}
+        }
+    )
+
+    print(miner)
+
 
 def main():
     urls = urlparser.list_project_urls("./sonar_measures.csv")
